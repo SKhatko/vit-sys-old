39f03929fb6f4f4a1cb617464cc6b74ec7a8d2a9
Stanislav Khatko <s.khatko@vitisch.com>
2017-12-24 14:57:00 +0200

    Fix

--- resources/views/restaurant/menu_categories/create.blade.php
+++ resources/views/restaurant/menu_categories/create.blade.php
@@ -3,26 +3,15 @@
 @section('content')
 
     <div class="restaurant-category">
-        @if (count($menuLanguages))
-            <div class="restaurant-category__header">
+        <div class="restaurant-category__header">
 
-                @include('components.back', ['text' => 'Back'])
+            @include('components.back', ['text' => 'Back'])
 
-            </div>
+        </div>
 
-            {{ Form::model($category = new \App\MenuCategory, ['action' => 'MenuCategoriesController@store', 'method' => 'POST', 'files' => true]) }}
-            @include('restaurant.menu_categories._form', ['submitButtonText' => trans('restaurant.create_category')])
-            {{ Form::close() }}
-
-        @else
-
-            @include('partials.error', [
-      'message' => trans('restaurant.add_languages_first_error'),
-      'action' => '',
-      'actionName' => 'Settings'
-       ])
-
-        @endif
+        {{ Form::model($category = new \App\MenuCategory, ['action' => 'MenuCategoriesController@store', 'method' => 'POST', 'files' => true]) }}
+        @include('restaurant.menu_categories._form', ['submitButtonText' => trans('restaurant.create_category')])
+        {{ Form::close() }}
 
     </div>
 
